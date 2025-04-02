17d2fe22c223a37e13cfec25cf33ae52e00da3b9
Anna Luiza <140816415+annakailer@users.noreply.github.com>
2025-04-02 11:26:08 +0000

    .

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     let tamanhoAtualFonte = 1;
     const aumentFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte')
 
     aumentFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+    diminuirFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
 })
